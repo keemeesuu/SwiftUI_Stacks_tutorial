3cbe7b442a70c6fe4a2a7eab1f070996adee625a
keemeesuu <kimhogy@gmail.com>
2021-05-20 22:28:05 +0900

    Initial Commit

---
+++ SwiftUI_Stacks_tutorial/SwiftUI_Stacks_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Stacks_tutorialApp.swift
+//  SwiftUI_Stacks_tutorial
+//
+//  Created by hogy on 2021/05/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Stacks_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
